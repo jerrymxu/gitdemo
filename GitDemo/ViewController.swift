cab5f5d930e7617320b989bdbb6265e72815b087
jerrymxu <mingming20000419@me.com>
2017-06-06 10:53:55 -0700

    Add the button and label

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func button(_ sender: Any) {
+        label.text = "Hello World!"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
